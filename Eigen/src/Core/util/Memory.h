0c0591a85510cc584e702c89d4342da0b38d2f60
Charles Schlosser <charless@vt.edu>
2022-11-18 01:40:11 +0000

    Update Memory.h

--- Eigen/src/Core/util/Memory.h
+++ Eigen/src/Core/util/Memory.h
@@ -135,12 +135,12 @@ EIGEN_DEVICE_FUNC inline void* handmade_aligned_realloc(void* ptr, std::size_t n
   if (original == old_original) return ptr;
   uint8_t offset = static_cast<uint8_t>(alignment - (reinterpret_cast<std::size_t>(original) & (alignment - 1)));
   void* aligned = static_cast<void*>(static_cast<uint8_t*>(original) + offset);
-  *(static_cast<uint8_t*>(aligned) - 1) = offset;
   if (offset != old_offset) {
     const void* src = static_cast<const void*>(static_cast<uint8_t*>(original) + old_offset);
     std::size_t count = (std::min)(new_size, old_size);
     std::memmove(aligned, src, count);
   }
+  *(static_cast<uint8_t*>(aligned) - 1) = offset;
   return aligned;
 }
 
