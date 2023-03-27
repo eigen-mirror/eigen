3c97dd09174e9ecc8924f40e34c3e7544ff2e447
Charles Schlosser <charless@vt.edu>
2023-03-27 17:31:08 +0000

    enable non-const ref with symbolic index

--- Eigen/src/plugins/IndexedViewMethods.h
+++ Eigen/src/plugins/IndexedViewMethods.h
@@ -168,12 +168,19 @@ operator()(const Indices& indices) EIGEN_INDEXED_VIEW_METHOD_CONST
             (derived(), internal::first(actualIndices), internal::index_list_size(actualIndices));
 }
 
-template<typename IndexType>
-std::enable_if_t<symbolic::is_symbolic<IndexType>::value, CoeffReturnType >
-operator()(const IndexType& id) EIGEN_INDEXED_VIEW_METHOD_CONST
-{
-  return Base::operator()(internal::eval_expr_given_size(id,size()));
+#ifndef EIGEN_INDEXED_VIEW_METHOD_2ND_PASS
+// const version
+template <typename IndexType>
+std::enable_if_t<symbolic::is_symbolic<IndexType>::value, CoeffReturnType> operator()(const IndexType& id) const {
+  return Base::operator()(internal::eval_expr_given_size(id, size()));
 }
+#else
+// non-const version
+template <typename IndexType>
+std::enable_if_t<symbolic::is_symbolic<IndexType>::value, Scalar&> operator()(const IndexType& id) {
+  return Base::operator()(internal::eval_expr_given_size(id, size()));
+}
+#endif
 
 template<typename IndicesT, std::size_t IndicesN>
 std::enable_if_t<IsRowMajor,
