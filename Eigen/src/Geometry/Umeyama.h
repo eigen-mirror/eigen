af3ca50f0b895c45e72130312cf9e78ba162454f
Rasmus Munk Larsen <rmlarsen@google.com>
2023-05-09 18:53:56 +0000

    Work around compiler bug in Umeyama.h.
    (cherry picked from commit 524c329ab23fb565eff8aa2bf36d134e97773a99)

--- Eigen/src/Geometry/Umeyama.h
+++ Eigen/src/Geometry/Umeyama.h
@@ -137,7 +137,8 @@ umeyama(const MatrixBase<Derived>& src, const MatrixBase<OtherDerived>& dst, boo
   VectorType S = VectorType::Ones(m);
 
   if  ( svd.matrixU().determinant() * svd.matrixV().determinant() < 0 )
-    S(m-1) = -1;
+    Index tmp = m - 1;  
+    S(tmp) = -1;
 
   // Eq. (40) and (43)
   Rt.block(0,0,m,m).noalias() = svd.matrixU() * S.asDiagonal() * svd.matrixV().transpose();
