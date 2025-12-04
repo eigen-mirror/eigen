595bd27621f03532f641f77a7d959453a1e1c972
Charlie Schlosser <cs.schlosser@gmail.com>
2025-12-04 00:08:53 -0500

    improvements

--- test/integer_division.cpp
+++ test/integer_division.cpp
@@ -9,26 +9,76 @@
 
 #include "main.h"
 
-template <typename T>
-void test_generic_magic() {
-  constexpr int k = 8 * sizeof(T);
-  int max_iter = 0;
-  T max_d = 1;
-  for (T d = 1; d != 0; d++) {
-    iter = 0;
-    int p = internal::log2_ceil(d);
-    T magic = internal::calc_magic_generic(d, p);
-    T ref = internal::calc_magic(d, p);
-    VERIFY_IS_EQUAL(magic, ref);
-  }
+template <typename Numerator, typename Divisor = Numerator>
+void test_division() {
+  using UnsignedNumerator = typename std::make_unsigned<Numerator>::type;
+  using UnsignedDivisor = typename std::make_unsigned<Divisor>::type;
+  using PlainType = VectorX<Numerator>;
+  using FastDivOp = internal::fast_div_op<Numerator>;
+  using FastDivXpr = CwiseUnaryOp<FastDivOp, PlainType>;
+  using RefRhs = typename internal::plain_constant_type<PlainType>::type;
+  using RefXpr = CwiseBinaryOp<internal::scalar_quotient_op<Numerator>, PlainType, RefRhs>;
 
+  Index size = 4096;
+  PlainType numerator(size);
+  for (int repeat = 0; repeat < EIGEN_TEST_MAX_SIZE; repeat++) {
+    numerator.setRandom();
+    Divisor d = internal::random<Divisor>(1, NumTraits<Divisor>::highest() / 4);
+    {
+      FastDivXpr xpr(numerator, FastDivOp(d));
+      for (Index i = 0; i < size; i++) {
+        UnsignedNumerator absNumerator = numext::abs(numerator.coeff(i));
+        Numerator ref = absNumerator / d;
+        if (numerator.coeff(i) < 0) ref = -ref;
+        VERIFY_IS_EQUAL(xpr.coeff(i), ref);
+      }
+    }
+    if (std::is_signed<Divisor>::value) {
+      Divisor neg_d = 0 - d;
+      FastDivXpr xpr(numerator, FastDivOp(neg_d));
+      for (Index i = 0; i < size; i++) {
+        UnsignedNumerator absNumerator = numext::abs(numerator.coeff(i));
+        Numerator ref = absNumerator / d;
+        if (numerator.coeff(i) > 0) ref = -ref;
+        VERIFY_IS_EQUAL(xpr.coeff(i), ref);
+      }
+    }
+  }
+}
+
+template <typename Numerator, bool Signed = std::is_signed<Numerator>::value>
+struct test_division_driver {
+  static void run() {
+    // divide unsigned numerators by unsigned divisors only
+    test_division<Numerator, uint8_t>();
+    test_division<Numerator, uint16_t>();
+    test_division<Numerator, uint32_t>();
+    test_division<Numerator, uint64_t>();
+  }
+};
+template <typename Numerator>
+struct test_division_driver<Numerator, true> {
+  static void run() {
+    // divide signed numerators by unsigned and signed divisors
+    test_division_driver<Numerator, false>::run();
+    test_division<Numerator, int8_t>();
+    test_division<Numerator, int16_t>();
+    test_division<Numerator, int32_t>();
+    test_division<Numerator, int64_t>();
+  }
+};
+template <typename Numerator>
+void run_division_tests() {
+  test_division_driver<Numerator>::run();
 }
 
 EIGEN_DECLARE_TEST(integer_division) {
-  CALL_SUBTEST_1(test_generic_magic<uint8_t>());
-  //CALL_SUBTEST_1(test_generic_magic<uint16_t>());
-  //CALL_SUBTEST_1(test_generic_magic<uint32_t>());
-  // for (int i = 0; i < g_repeat; i++) {
-  //   // CALL_SUBTEST_1((test_realview<Dynamic, Dynamic, Dynamic, Dynamic>()));
-  // }
+  CALL_SUBTEST_1(run_division_tests<uint8_t>());
+  CALL_SUBTEST_1(run_division_tests<uint16_t>());
+  CALL_SUBTEST_1(run_division_tests<uint32_t>());
+  CALL_SUBTEST_1(run_division_tests<uint64_t>());
+  CALL_SUBTEST_1(run_division_tests<int8_t>());
+  CALL_SUBTEST_1(run_division_tests<int16_t>());
+  CALL_SUBTEST_1(run_division_tests<int32_t>());
+  CALL_SUBTEST_1(run_division_tests<int64_t>());
 }
