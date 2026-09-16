5fda14f46305e4341f309cbcb4fcd99ffb34ebf2
Rasmus Munk Larsen <rmlarsen@gmail.com>
2026-09-16 02:24:59 -0700

    CI: Log machine configuration before builds and tests

--- ci/scripts/common.windows.before_script.ps1
+++ ci/scripts/common.windows.before_script.ps1
@@ -3,6 +3,13 @@
 
 echo "Running ${CI_JOB_NAME}"
 
+echo "Host machine configuration:"
+Get-CimInstance Win32_OperatingSystem -ErrorAction Continue |
+  Select-Object Caption, Version, OSArchitecture, TotalVisibleMemorySize, FreePhysicalMemory | Format-List
+Get-CimInstance Win32_Processor -ErrorAction Continue |
+  Select-Object Name, NumberOfCores, NumberOfLogicalProcessors, MaxClockSpeed | Format-List
+Get-PSDrive -PSProvider FileSystem | Format-Table Name, Used, Free -AutoSize
+
 # Print configuration variables.
 Get-Variable EIGEN* | Format-Table -Wrap
 Get-Variable CMAKE* | Format-Table -Wrap
